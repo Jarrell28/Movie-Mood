3e8f1a4936327076ee1ec69525f7a3ca640cfc50
Lia879 <54521457+lia879@users.noreply.github.com>
2020-01-02 18:16:41 -0600

    adding codes to back end
    -when selecting mood, need to display movie result... which i did on the back end part ... so you would typed localhost:3000/funny or romantic or whatever your mood is in after the forward slash 
    -need to put moods in a database and then display the moods on the page from the database

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
+var path = require("path");
 
 var db = require("./models");
 
@@ -11,6 +12,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Handlebars
 app.engine(
@@ -34,8 +36,8 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync(syncOptions).then(function () {
+  app.listen(PORT, function () {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
