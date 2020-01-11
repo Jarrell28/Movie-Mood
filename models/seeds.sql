4ced82430cca49d688be590db1cf2273dbba7116
Lia879 <54521457+lia879@users.noreply.github.com>
2020-01-11 12:21:38 -0600

    adding more buttons

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,21 +1,43 @@
 use project2;
 
 INSERT INTO moods
-    (mood, genre_ids)
+    (mood, genre_id)
 VALUES('funny', '35, 10751');
 
 INSERT INTO moods
-    (mood, genre_ids)
+    (mood, genre_id)
 VALUES('romantic', '10749');
 
 INSERT INTO moods
-    (mood, genre_ids)
+    (mood, genre_id)
 VALUES('uplifting', '14, 10751, 18, 35');
 
 INSERT INTO moods
-    (mood, genre_ids)
+    (mood, genre_id)
 VALUES('dark', '27, 9648, 80, 10752');
 
 INSERT INTO moods
-    (mood, genre_ids)
+    (mood, genre_id)
 VALUES('excited', '12, 16, 14, 878, 53, 28');
+
+
+
+INSERT INTO moods
+    (mood, genre_id)
+VALUES('challenging', '12, 10751, 10752, 36');
+
+INSERT INTO moods
+    (mood, genre_id)
+VALUES('emotional', '10749, 10751, 18, 99');
+
+INSERT INTO moods
+    (mood, genre_id)
+VALUES('instructive', '36, 99');
+
+INSERT INTO moods
+    (mood, genre_id)
+VALUES('thought-provoking', '9648, 10751, 18, 878');
+
+INSERT INTO moods
+    (mood, genre_id)
+VALUES('action-packed', '28, 12, 16, 53');
