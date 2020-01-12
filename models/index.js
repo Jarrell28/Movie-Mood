77573e549b423c3e1574255e968cef97117ad610
Jarrell Houston <jhouston2882@yahoo.com>
2020-01-12 17:04:07 -0600

    deploying to heroku

--- models/index.js
+++ models/index.js
@@ -8,10 +8,10 @@ var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.js")[env];
 var db = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else if (process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
   var sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(
     config.database,
